c025b47742d1f7c74de8f228c8768690c1d184b6
Taras Hamkalo <hamkalo.taras@gmail.com>
2024-11-18 11:30:31 +0100

    feat(endpoints): add error handler

--- src/main/python/app.py
+++ src/main/python/app.py
@@ -16,6 +16,10 @@ class App:
     self.solver: Solver = self.solvers['dfs']
 
   def get_steps(self, solver_name: str):
+    if solver_name not in self.solvers:
+      raise ValueError(f'Solver "{solver_name}" is not supported')
+
+    self.solver = self.solvers[solver_name]
     self.solver.reset()
     self.solver.solve()
     return self.solver.get_steps()
--- src/main/python/main.py
+++ src/main/python/main.py
@@ -5,10 +5,15 @@ from app import App
 flask = Flask(__name__)
 app = App()
 
+@flask.errorhandler(ValueError)
+def handle_bad_request(e: Exception):
+  return str(e), 400
+
 @flask.route('/board/<string:solver_name>/solve', methods=['GET'])
 def get_steps(solver_name: str):
   print(solver_name)
   return app.get_steps(solver_name)
+
 
 if __name__ == '__main__':
   flask.run()
