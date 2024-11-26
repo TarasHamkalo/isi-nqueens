5d11f6f0a87b4c32b3454c9e0ee13c19b57c7eb3
Taras Hamkalo <hamkalo.taras@gmail.com>
2024-11-26 16:01:27 +0100

    refactor: hill climbing

--- src/main/python/solver/hill_climbing.py
+++ src/main/python/solver/hill_climbing.py
@@ -1,3 +1,4 @@
+import logging
 import random
 from typing import List
 
@@ -15,6 +16,7 @@ class Neighbor:
         self.heuristic_score: int = heuristic_score
 
 class HillClimbing(Solver):
+
     def __init__(self, n):
         super().__init__(n)
         self.queens = [-1] * self.n  # track which queen and where (row: col)
@@ -22,6 +24,8 @@ class HillClimbing(Solver):
         self.diag = [0] * (2 * self.n - 1)  # track number of queens in each main diagonals
         self.anti_diag = [0] * (2 * self.n - 1)  # track number of queens in each reverse diagonal
         self.post_init()
+        self.updates_count = 0
+        self.solution_score = -1
 
     def post_init(self):
         for row in range(self.n):
@@ -30,39 +34,67 @@ class HillClimbing(Solver):
             self._put_queen(row, col)
 
     def solve(self):
-        self._solve(limit=500)
+        self._solve(limit=int(1e32), side_moves=100)
 
-    def _solve(self, limit):
-        current_score = self.get_number_of_attacking_queens()
+    def _solve(self, limit: int, side_moves: int):
+        side_moves_copy = side_moves
+
+        self.solution_score = self.get_number_of_attacking_queens()
         for i in range(limit):
+            if self.solution_score == 0:
+                logging.debug("Solution was found!")
+                return
+
             neighbors = self.get_neighbors()
 
-            print(f"Iteration {i}: Evaluating neighbors...")
+            logging.debug(f"Iteration {i}: Evaluating neighbors...")
+
             best_neighbor: Neighbor = min(neighbors, key=lambda n: n.heuristic_score)
-            print(f"Best score is {best_neighbor.heuristic_score}."
+            logging.debug([neighbor.heuristic_score for neighbor in neighbors])
+            logging.debug(f"Best score is {best_neighbor.heuristic_score}."
                   f" Move Queen {best_neighbor.queen} to Column {best_neighbor.next_col}")
 
-            self.print_board()
-            self.print_neighbors(neighbors)
+            # self.print_board()
+            # self.print_neighbors(neighbors)
 
-            if best_neighbor.heuristic_score < current_score:
-                print(f"Updating score from {current_score} to {best_neighbor.heuristic_score}")
-                current_score = best_neighbor.heuristic_score
-
-                # Update the queen's position
-                self._remove_queen(best_neighbor.queen)
-                self._put_queen(best_neighbor.queen, best_neighbor.next_col)
-
-                # Update the board
-                self.board[best_neighbor.queen][best_neighbor.orig_col] = EMPTY
-                self.board[best_neighbor.queen][best_neighbor.next_col] = QUEEN
-
-                # Record steps
-                self.steps.append((best_neighbor.queen, best_neighbor.orig_col))
-                self.steps.append((best_neighbor.queen, best_neighbor.next_col))
+            if best_neighbor.heuristic_score < self.solution_score:
+                side_moves_copy = side_moves
+                self.solution_score = self.update_state(self.solution_score, best_neighbor)
+            elif best_neighbor.heuristic_score == self.solution_score and side_moves_copy > 0:
+                logging.debug("Making side move!")
+                side_moves_copy -= 1
+                self.solution_score = self.update_state(self.solution_score, best_neighbor)
             else:
-                print("No improvement possible. Stopping.")
+                logging.debug("No improvement possible.")
+
                 return
+
+    def update_state(self, current_score, best_neighbor):
+        logging.debug(f"Updating score from {current_score} to {best_neighbor.heuristic_score}")
+        self.updates_count += 1
+        # Update the queen's position
+        self._remove_queen(best_neighbor.queen)
+        self._put_queen(best_neighbor.queen, best_neighbor.next_col)
+
+        # Update the board
+        self.board[best_neighbor.queen][best_neighbor.orig_col] = EMPTY
+        self.board[best_neighbor.queen][best_neighbor.next_col] = QUEEN
+
+        # Record steps
+        self.steps.append((best_neighbor.queen, best_neighbor.orig_col))
+        self.steps.append((best_neighbor.queen, best_neighbor.next_col))
+
+        return best_neighbor.heuristic_score
+
+    def reset(self):
+        super().reset()
+        self.queens = [-1] * self.n
+        self.columns = [0] * self.n
+        self.diag = [0] * (2 * self.n - 1)
+        self.anti_diag = [0] * (2 * self.n - 1)
+        self.post_init()
+        self.updates_count = 0
+        self.solution_score = -1
 
     def print_neighbors(self, neighbors):
         for i, neighbor in enumerate(neighbors):
@@ -96,7 +128,7 @@ class HillClimbing(Solver):
                 if orig_col == next_col:
                     continue
 
-                print(f"Move queen {queen} from {orig_col} to {next_col}")
+                # print(f"Move queen {queen} from {orig_col} to {next_col}")
                 self._remove_queen(queen)
                 self._put_queen(queen, next_col)
                 heuristic_score = self.get_number_of_attacking_queens()
@@ -119,8 +151,8 @@ class HillClimbing(Solver):
         col_attacking_queens = sum(q * (q - 1) // 2 for q in self.columns)
         diag_attacking_queens = sum(q * (q - 1) // 2 for q in self.diag)
         anti_diag_attacking_queens = sum(q * (q - 1) // 2 for q in self.anti_diag)
-        print("Columns:", self.columns, "Attacks:", col_attacking_queens)
-        print("Diagonals:", self.diag, "Attacks:", diag_attacking_queens)
-        print("Anti-diagonals:", self.anti_diag, "Attacks:", anti_diag_attacking_queens)
+        logging.debug(f"Columns: {self.columns}, attacks {col_attacking_queens}")
+        logging.debug(f"Diagonals: {self.diag}, attacks {diag_attacking_queens}")
+        logging.debug(f"Anti-diagonals:: {self.anti_diag}, attacks {anti_diag_attacking_queens}")
 
         return col_attacking_queens + diag_attacking_queens + anti_diag_attacking_queens
